58d873995b9066a6a99773d3803257c3e6c54a97
Cak-eng-laden <82126908+Cak-eng-laden@users.noreply.github.com>
2021-05-30 22:01:46 +0700

    Update tambah_pengunjung.php

--- sistem informasi TOEFL/admin/process/tambah_pengunjung.php
+++ sistem informasi TOEFL/admin/process/tambah_pengunjung.php
@@ -6,8 +6,9 @@ include("../sys/koneksi.php");
 // }
         $nama = $_POST["nama"];
         $number = $_POST["number"];
+        $email = $_POST["email"];
 
-        $insert = mysqli_query($conn, "INSERT INTO `tes` (`id_tes`, `no_telpon`, `nama`, `score`, `sisa_waktu`, `id_user`, `selesai`) VALUES (NULL, '$number', '$nama', NULL, NULL, '2', NULL);");
+        $insert = mysqli_query($conn, "INSERT INTO `tes` (`id_tes`, `no_telpon`, `nama`,`email`, `score`, `sisa_waktu`, `id_user`, `selesai`) VALUES (NULL, '$number', '$nama', '$email',NULL, NULL, '2', NULL);");
         if($insert){
             ?>
             <script>
