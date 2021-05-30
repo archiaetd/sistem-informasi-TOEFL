d8f2e8b5568e3832f2d0640b5aaf63c9d7e2a2d3
Cak-eng-laden <82126908+Cak-eng-laden@users.noreply.github.com>
2021-05-30 21:34:06 +0700

    Update score.php

--- sistem informasi TOEFL/score.php
+++ sistem informasi TOEFL/score.php
@@ -2,7 +2,10 @@
 
 include("admin/sys/koneksi.php");
 
-$select = mysqli_query($conn, "select cek_soal.*, soal.kunci from cek_soal INNER JOIN soal ON cek_soal.id_soal = soal.id_soal where id_tes = 111");
+$idtes = mysqli_query($conn, "select max(id_tes) FROM tes");
+$idnya = mysqli_fetch_array($idtes);
+
+$select = mysqli_query($conn, "select cek_soal.*, soal.kunci, soal.tipe_soal from cek_soal INNER JOIN soal ON cek_soal.id_soal = soal.id_soal where id_tes = $idnya[0] AND tipe_soal = 'reading' ");
 
 $score = 0;
 while($data=mysqli_fetch_array($select)){
@@ -10,6 +13,41 @@ while($data=mysqli_fetch_array($select)){
     $score += 10;
   }
 }
+$level = "";
+if ($score >= 80) {
+   $level = "A";
+}elseif ($score >= 70) {
+   $level = "B";
+}elseif ($score >= 60) {
+   $level = "C";
+}elseif ($score >= 50) {
+   $level = "D";
+}else{
+  $level = "E";
+}
+
+$select1 = mysqli_query($conn, "select cek_soal.*, soal.kunci, soal.tipe_soal from cek_soal INNER JOIN soal ON cek_soal.id_soal = soal.id_soal where id_tes = $idnya[0] AND tipe_soal = 'listening' ");
+
+$score1 = 0;
+while($data=mysqli_fetch_array($select1)){
+  if($data["jawaban"]==$data["kunci"]){
+    $score1 += 10;
+  }
+}
+$level1 = "";
+if ($score1 >= 80) {
+   $level1 = "A";
+}elseif ($score1 >= 70) {
+   $level1 = "B";
+}elseif ($score1 >= 60) {
+   $level1 = "C";
+}elseif ($score1 >= 50) {
+   $level1 = "D";
+}else{
+  $level1 = "E";
+}
+$scorenya = $score+$score1;
+$select = mysqli_query($conn, "update tes set score = '$scorenya' where id_tes = $idnya[0]");
 
 ?>
 
@@ -45,13 +83,13 @@ while($data=mysqli_fetch_array($select)){
     </tr>
     <tr style="background-color: linen; font-size: 20px;">
     <td width="150" align="center">Reading</td>
-    <td width="200" align="center"><?php echo $score; ?></td>
-    <td width="200" align="center"></td>
+    <td width="200" align="center"><?php echo $score;?></td>
+    <td width="200" align="center"><?php echo $level; ?></td>
     </tr>
     <tr style="background-color: linen; font-size: 20px;">
     <td width="150" align="center">Listening</td>
-    <td width="200" align="center"></td>
-    <td width="200" align="center"></td>
+    <td width="200" align="center"><?php echo $score1; ?></td>
+    <td width="200" align="center"><?php echo $level1; ?></td>
     </tr>
     <a href="" class="btn btn-primary start-0" style="margin-left: 2rem;">Share it</a>
 
