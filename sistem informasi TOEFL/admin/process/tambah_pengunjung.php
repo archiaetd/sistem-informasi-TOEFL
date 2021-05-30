64c43c33a831553201fc6990b08aa37623117693
Cak-eng-laden <82126908+Cak-eng-laden@users.noreply.github.com>
2021-05-30 21:51:43 +0700

    Create tambah_pengunjung.php

---
+++ sistem informasi TOEFL/admin/process/tambah_pengunjung.php
@@ -0,0 +1,20 @@
+<?php
+
+include("../sys/koneksi.php");
+// if(!isset($_SESSION["user"])){
+//     header("location:login.php");
+// }
+        $nama = $_POST["nama"];
+        $number = $_POST["number"];
+
+        $insert = mysqli_query($conn, "INSERT INTO `tes` (`id_tes`, `no_telpon`, `nama`, `score`, `sisa_waktu`, `id_user`, `selesai`) VALUES (NULL, '$number', '$nama', NULL, NULL, '2', NULL);");
+        if($insert){
+            ?>
+            <script>
+                document.location = "tambah_jawaban.php";
+            </script>
+            <?php
+        }else{
+            echo "error";
+        }
+?>
